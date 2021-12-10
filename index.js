0486b7efd2fa209b3fc766dce3329f4c493fa2dd
Eric Yan <yaneric.4d@gmail.com>
2021-12-10 01:49:41 -0700

    added deploy-commands.js to listen for commands

--- index.js
+++ index.js
@@ -8,4 +8,18 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
 });
 
+client.on('interactionCreate', async interaction => {
+    if(!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if(commandName === 'ping') {
+        await interaction.reply('Hi');
+    } else if(commandName === 'join') {
+        await interaction.reply('Joining...');
+    }
+
+});
+
+
 client.login(token);
